3aec5ac0373e6fbb6bc30b7f9d35e3418e912083
Akarsh Rana <vanshrana2012@gmail.com>
2020-06-08 14:28:52 +0530

    allauth path added

--- connectSABI/connectSABI/urls.py
+++ connectSABI/connectSABI/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('befLogn.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
